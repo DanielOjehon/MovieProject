69f99315334cc3f7ddb291f5daab0d702dad3877
DanielOjehonmon-STUD <DanielOjehonmon-STUD@S-9S785R3>
2026-01-05 17:59:03 +0000

    third

--- MovieProject/ViewModel/MovieDetailsViewModel.cs
+++ MovieProject/ViewModel/MovieDetailsViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MovieProject.ViewModel
 {
-    internal class MovieDetailsViewModel:BaseViewModel
+    internal class MovieDetailsViemodel:BaseViewModel
     {
     }
 }
