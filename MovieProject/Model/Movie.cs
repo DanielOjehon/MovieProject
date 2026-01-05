520c12d94c1de1b18d5eab909529e837b896947c
DanielOjehonmon-STUD <DanielOjehonmon-STUD@S-9S785R3>
2026-01-05 17:53:11 +0000

    test

--- MovieProject/Model/Movie.cs
+++ MovieProject/Model/Movie.cs
@@ -8,13 +8,14 @@ namespace MovieProject.Model
 {
     public class Movie
     {
-         
+
         public string title { get; set; }
         public int year { get; set; }
         public List<string> genre { get; set; }
         public string director { get; set; }
         public double rating { get; set; }
         public string emoji { get; set; }
-    
-}
+
+
+    }
 }
---
+++ MovieProject/Services/MovieService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MovieProject.Model;
+
+
+namespace MovieProject.Services
+{
+    public class MovieService
+    {
+        List<Movie> movieList = new List<Movie>();
+
+        public Task<List<Movie>> GetMovies()
+        {
+            return Task.FromResult(movieList);
+        }
+    }
+}
