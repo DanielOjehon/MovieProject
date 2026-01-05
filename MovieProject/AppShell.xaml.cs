756934d2d6111553fa19d40990e1472ceb3d3806
DanielOjehonmon-STUD <DanielOjehonmon-STUD@S-9S785R3>
2026-01-05 11:48:33 +0000

    Add project files.

---
+++ MovieProject/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieProject
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MovieProject/Model/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieProject.Model
+{
+    public class Movie
+    {
+         
+        public string title { get; set; }
+        public int year { get; set; }
+        public List<string> genre { get; set; }
+        public string director { get; set; }
+        public double rating { get; set; }
+        public string emoji { get; set; }
+    
+}
+}
