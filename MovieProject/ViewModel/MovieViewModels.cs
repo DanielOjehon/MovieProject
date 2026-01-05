5347b28ab19d6e25ebd75d006cd9fa4209d9bdd6
DanielOjehonmon-STUD <DanielOjehonmon-STUD@S-9S785R3>
2026-01-05 19:20:16 +0000

    ViewModel

--- MovieProject/ViewModel/MovieViewModels.cs
+++ MovieProject/ViewModel/MovieViewModels.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
-
+using MovieProject.Services;
 using MovieProject.Model;
 
 namespace MovieProject.ViewModel
@@ -11,10 +11,30 @@ namespace MovieProject.ViewModel
     
     public partial class MovieViewModels:BaseViewModel
      {
+        MovieService movieService;
         public ObservableCollection<Movie> Movies { get; } = new();
-        public MovieViewModels()
+        public MovieViewModels(MovieService movieService)
         {
             Title = "MovieApp";
+            this.movieService = movieService;
+        }
+        async Task GetMoviesAsync()
+        {
+            if (IsBusy)
+                return;
+            try
+            {
+                IsBusy = true;
+                
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                IsBusy = false;
+            }
         }
     }
 }
